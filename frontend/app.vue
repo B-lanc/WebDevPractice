24b3ae327bfa71709865b635c71ca330979c371a
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-24 05:12:53 +0700

    Environment variable for DOMAIN

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import PocketBase from "pocketbase"
+
+const config = useRuntimeConfig()
+const domain = config.public.domain
+const port = domain === "localhost" ? 8080 : 8443
+const pb = new PocketBase(`http://${domain}:${port}`)
+
+</script>
+
 <template>
   <div>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
+  <div>{{ domain }}</div>
+  <div>{{ port }}</div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+  ],
+  runtimeConfig: {
+    public: {
+      domain: process.env.DOMAIN ? process.env.DOMAIN : "localhost"
+    }
+  }
 })
